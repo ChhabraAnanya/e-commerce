921908e902d9e5479bfb819ae2c19cfe1c33187d
ChhabraAnanya <ananya05chhabra@gmail.com>
2024-10-04 22:41:45 +0100

    Add files via upload

---
+++ mens_tshirt.jsx
@@ -0,0 +1,332 @@
+export const mens_tshirt = [
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/fa/a7/faa71f7684c70f90d887416feba8c448f9120260.jpg?imwidth=396",
+    brand: "El Senor",
+    title: "Men Regular Rib-Fit Knitted T-shirt",
+    color: "grey",
+    discountedPrice: 299,
+    price: 1299,
+    discountPersent: 76,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/0a/65/0a659d3834f51e63328a409b10c5def3a2704cf5.jpg?imwidth=396",
+    brand: "youth first",
+    title: "Loose-fit T-Shirt",
+    color: "black",
+    discountedPrice: 299,
+    price: 899,
+    discountPersent: 66,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/e3/d4/e3d4d59016e36543c6fe3a779bc622df502cb376.jpg?imwidth=396",
+    brand: "VTEXX",
+    title: "Men Loose Fit T-Shirt",
+    color: "green",
+    discountedPrice: 299,
+    price: 1299,
+    discountPersent: 76,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/5a/df/5adfbef3367f5a9275fdc6168e36ae317f190ccd.jpg?imwidth=396",
+    brand: "Profi",
+    title: "Men Regular Fit Collar Shirt",
+    color: "white",
+    discountedPrice: 879,
+    price: 1599,
+    discountPersent: 45,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/f4/10/f410b50281a96fb222cdca2286be1218bbc73870.jpg?imwidth=396",
+    brand: "FUBAR",
+    title: "Men Graphic T-Shirt",
+    color: "white",
+    discountedPrice: 419,
+    price: 1648,
+    discountPersent: 74,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/f7/27/f72751d0bf611298a04004f01036afc0420e6069.jpg?imwidth=396",
+    brand: "H&M",
+    title: "Men Slim Vest Top",
+    color: "white",
+    discountedPrice: 399,
+    price: 1299,
+    discountPersent: 69,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/59/e3/59e34ed27816e655f2105f41b3b829649d90e678.jpg?imwidth=396",
+    brand: "NoFap",
+    title: "Men Regular Fit Solid Button Down Collar Formal Shirt",
+    color: "white",
+    discountedPrice: 379,
+    price: 1849,
+    discountPersent: 79,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/1f/bf/1fbfc403571842bb6b1d64ffb8b94fbd83180d50.jpg?imwidth=396",
+    brand: "H&M",
+    title: "Loose-fit printed T-Shirt",
+    color: "black",
+    discountedPrice: 498,
+    price: 999,
+    discountPersent: 50,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/6c/34/6c34afb3ae99fad0ebcfefd0695734cfd93e5d31.jpg?imwidth=396",
+    brand: "3BROS",
+    title: "Loose Fit printed T-Shirt ",
+    color: "white",
+    discountedPrice: 499,
+    price: 799,
+    discountPersent: 50,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/9e/95/9e9500e577be73c12f282a2c9d9216e5eadc1b35.jpg?imwidth=396",
+    brand: "3BROS",
+    title: "Regular Fit Jersey Top",
+    color: "navy",
+    discountedPrice: 499,
+    price: 1799,
+    discountPersent: 72,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+  {
+    imageUrl:
+      "https://image.hm.com/assets/hm/4e/d2/4ed27a25f8e5782be33a489bb46f50e20d82e3c2.jpg?imwidth=396",
+    brand: "STONEBERG",
+    title: "Greige, Solid color",
+    color: "white",
+    discountedPrice: 409,
+    price: 1599,
+    discountPersent: 74,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "shirt",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+];
